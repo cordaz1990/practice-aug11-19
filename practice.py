ad4908a63721e0add5fd0af65ad06640f66b47db
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 17:15:49 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,18 @@
-print('hello')
-print('world')
-print()
+#Reverse Lookup
+def reverse_lookup(d,v):
+    for k in d:
+      if d[k] == v:
+          return k
+        
+    raise lookupError()
+    
+#Dictionary
+def invert_dict(d):
+    inverse = dict()
+    for key in d:
+      val = d[key]
+      if val not in inverse:
+         inverse[val] = [key]
+      else:
+        inverse[val].append(key)
+    return inverse
